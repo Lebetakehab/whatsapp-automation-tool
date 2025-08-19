65498f6c11213be951ffa4ea0544ca31a96d4b57
Lebetakehab <ezmz858@gmail.com>
2025-08-19 16:00:26 +0300

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1755608426053

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_7137d75d._.js",
+  "static/chunks/src_app_providers_tsx_6f5abcf0._.js"
+],
+    source: "dynamic"
+});
